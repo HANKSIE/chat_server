6ec3ca7ae3a852f41bb35e99ad55849c459e306c
HANKSIE <32055793+HANKSIE@users.noreply.github.com>
2022-07-17 21:13:44 +0800

    add zh-TW lang files

---
+++ resources/lang/zh-TW/auth.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+     */
+
+    'failed' => '憑證與紀錄不符',
+    'password' => '錯誤的密碼',
+    'throttle' => '嘗試登入錯誤過多，請於 :seconds 秒後再嘗試',
+    'email_verified' => '信箱已驗證',
+    'verification_link_sent' => '已發送驗證連結',
+];
---
+++ resources/lang/zh-TW/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+     */
+
+    'reset' => '您的密碼已重置!',
+    'sent' => '已寄送更改密碼連結',
+    'throttled' => '請稍後再重試',
+    'token' => '無效的密碼重置令牌',
+    'user' => "沒有該名使用者",
+
+];
