f3c783375c4b8d1cd9656e4a53f02140297bba08
HANKSIE <32055793+HANKSIE@users.noreply.github.com>
2022-08-06 09:22:36 +0800

    fix re-declare error when test

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -30,30 +30,31 @@ class UserSeeder extends Seeder
             'email' => "hanksie@gmail.com",
             'password' => Hash::make('iamfaker'),
         ]);
-        function makeFriend($user1, $user2)
-        {
-            DB::transaction(function () use ($user1, $user2) {
-                $group = Group::create(['is_one_to_one' => true]);
-                $group->members()->attach($user1);
-                $group->members()->attach($user2);
-                MessageRead::create(['user_id' => $user1->id, 'group_id' => $group->id]);
-                MessageRead::create(['user_id' => $user2->id, 'group_id' => $group->id]);
-                $user1->friends()->attach($user2, ['group_id' => $group->id]);
-                $user2->friends()->attach($user1, ['group_id' => $group->id]);
-                collect([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])->each(function ($data) use ($group, $user2) {
-                    tap($group->messages()->create(['body' => $data]), function ($message) use ($user2) {
-                        $message->user()->associate($user2);
-                        $message->save();
-                    });
-                });
-            });
-        }
 
         $user = User::find(1);
         $friends = User::whereBetween('id', [15, 16])->get();
         $friends->each(function ($friend) use ($user) {
-            makeFriend($user, $friend);
+            $this->makeFriend($user, $friend);
         });
         FriendRequest::create(['sender_id' => 17, 'recipient_id' => 1]);
+    }
+
+    public function makeFriend($user1, $user2)
+    {
+        DB::transaction(function () use ($user1, $user2) {
+            $group = Group::create(['is_one_to_one' => true]);
+            $group->members()->attach($user1);
+            $group->members()->attach($user2);
+            MessageRead::create(['user_id' => $user1->id, 'group_id' => $group->id]);
+            MessageRead::create(['user_id' => $user2->id, 'group_id' => $group->id]);
+            $user1->friends()->attach($user2, ['group_id' => $group->id]);
+            $user2->friends()->attach($user1, ['group_id' => $group->id]);
+            collect([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])->each(function ($data) use ($group, $user2) {
+                tap($group->messages()->create(['body' => $data]), function ($message) use ($user2) {
+                    $message->user()->associate($user2);
+                    $message->save();
+                });
+            });
+        });
     }
 }
